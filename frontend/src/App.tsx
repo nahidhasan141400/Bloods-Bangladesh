5b59765a9272159f91fbacc5b3d17cfe4358fff2
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2024-08-18 12:20:51 +0600

    update App.tsx and Home.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
+import { ConfigProvider } from "antd";
 import Router from "./routes/Router";
 
 function App() {
   return (
-    <>
+    <ConfigProvider>
       <Router />
-    </>
+    </ConfigProvider>
   );
 }
 
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,10 +1,11 @@
+import { Button } from "antd";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="flex h-screen w-full justify-center items-center">
+      <Button>Click Me</Button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
