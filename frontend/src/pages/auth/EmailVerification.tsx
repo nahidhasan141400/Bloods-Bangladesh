29dda7d87b677d2b42c954a680c9f91094767cc6
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2024-08-18 12:31:00 +0600

    create 1 file and update 3 files

---
+++ frontend/src/pages/auth/EmailVerification.tsx
@@ -0,0 +1,5 @@
+const EmailVerification = () => {
+  return <div>Email Verification</div>;
+};
+
+export default EmailVerification;
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import EmailVerification from "../pages/auth/EmailVerification";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/auth">
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="email_verification" element={<EmailVerification />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
