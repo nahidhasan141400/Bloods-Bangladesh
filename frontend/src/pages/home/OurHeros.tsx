65109d9d6e7977be46f40b64bbea140093fa283d
Nahid Hasan <63138743+nahidhasan141400@users.noreply.github.com>
2024-08-20 00:50:56 +0600

    update 2 files and delete 1 file

--- frontend/src/pages/home/OurHeros.tsx
+++ frontend/src/pages/home/OurHeros.tsx
@@ -9,55 +9,61 @@ const OurHeros = () => {
         </h1>
       </div>
       <div className="w-[80%] m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-1 mt-10">
+        {/* start  */}
         <div className="flex flex-col items-center">
           <img
-            className="rounded-full object-cover mb-1 size-32 md:size-48"
+            className="rounded-full object-cover mb-1 size-24 md:size-32 border-2 border-red-500"
             src="/images/heroImg.webp"
             alt="girl"
           />
-          <h1 className="text-xl font-medium">Jenny</h1>
+          <h1 className="text-xl font-medium">Mugdho</h1>
           <p className="text-sm">5 times</p>
         </div>
-
-        <div className=" flex flex-col items-center">
-          <img
-            className="rounded-full object-cover mb-1 size-32 md:size-48"
-            src="/images/heroImg.webp"
-            alt="girl"
-          />
-          <h1 className="text-xl font-medium">Jenny</h1>
-          <p className="text-sm">5 times</p>
-        </div>
-
+        {/* end  */}
+        {/* start  */}
         <div className="flex flex-col items-center">
           <img
-            className="rounded-full object-cover mb-1 size-32 md:size-48"
+            className="rounded-full object-cover mb-1 size-24 md:size-32 border-2 border-red-500"
             src="/images/heroImg.webp"
             alt="girl"
           />
-          <h1 className="text-xl font-medium">Jenny</h1>
+          <h1 className="text-xl font-medium">Mugdho</h1>
           <p className="text-sm">5 times</p>
         </div>
-
+        {/* end  */}
+        {/* start  */}
         <div className="flex flex-col items-center">
           <img
-            className="rounded-full object-cover mb-1 size-32 md:size-48"
+            className="rounded-full object-cover mb-1 size-24 md:size-32 border-2 border-red-500"
             src="/images/heroImg.webp"
             alt="girl"
           />
-          <h1 className="text-xl font-medium">Jenny</h1>
+          <h1 className="text-xl font-medium">Mugdho</h1>
           <p className="text-sm">5 times</p>
         </div>
-
+        {/* end  */}
+        {/* start  */}
         <div className="flex flex-col items-center">
           <img
-            className="rounded-full object-cover mb-1 size-32 md:size-48"
+            className="rounded-full object-cover mb-1 size-24 md:size-32 border-2 border-red-500"
             src="/images/heroImg.webp"
             alt="girl"
           />
-          <h1 className="text-xl font-medium">Jenny</h1>
+          <h1 className="text-xl font-medium">Mugdho</h1>
           <p className="text-sm">5 times</p>
         </div>
+        {/* end  */}
+        {/* start  */}
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full object-cover mb-1 size-24 md:size-32 border-2 border-red-500"
+            src="/images/heroImg.webp"
+            alt="girl"
+          />
+          <h1 className="text-xl font-medium">Mugdho</h1>
+          <p className="text-sm">5 times</p>
+        </div>
+        {/* end  */}
       </div>
     </Container>
   );
