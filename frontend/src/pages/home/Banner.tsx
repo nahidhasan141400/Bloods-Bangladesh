a498a890de65dddd332e37adab16ec023f8bc1f3
Nahid Hasan <63138743+nahidhasan141400@users.noreply.github.com>
2024-08-19 12:32:31 +0600

    create 2 files and update 1 file

---
+++ frontend/src/pages/home/Banner.tsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+    <div className="w-full relative">
+      <img
+        src="/banner.png"
+        alt="banner"
+        className="max-w-3xl mx-auto rounded-xl"
+      />
+    </div>
+  );
+};
+
+export default Banner;
